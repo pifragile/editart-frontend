067b111f2c9775c8565f766acccf8ae223f92744
pifragile <pigu@bluewin.ch>
2023-04-07 10:46:48 +0800

    remove overview on staging

--- src/contracts.js
+++ src/contracts.js
@@ -2,57 +2,14 @@ import { ENV } from "./consts";
 
 let contractList;
 if (["dev", "staging"].includes(ENV)) {
-    contractList = [
-        {
-            name: "Dash",
-            address: "KT1MxHSeeRa4bG5LoddwhXGzwcjvv4rLowNu",
-            author: "loackme",
-            showArtifact: true
-        },
-        {
-            name: "Shadows",
-            address: "KT1Cv9KEmxb2DCgM1Hdp9Fxuaxqz57WGkQrs",
-            author: "loackme",
-        },
-        {
-            name: "Interwoven",
-            address: "KT1SSX3W9Dzd1wxL4w99qoXcDwrBns9Cv4M1",
-            author: "MathBird",
-        },
-        {
-            name: "EATEST",
-            address: "KT1Rq14t5dfUxYkQXw1NGFciBxi9Spjm74mG",
-            author: "pifragile",
-        },
-        {
-            name: "PIXELFACE",
-            address: "KT1QcCcnTsZHR19DBQiFiddV2kv8q9fJudav",
-            author: "Filter8",
-        },
-        {
-            name: "Hybrid",
-            address: "KT1VwK23Bicp8Mr6LAze6WtZchkqSSF2kWaB",
-            author: "pifragile",
-            disableMintOnMobile: true,
-        },
-        {
-            name: "Brick",
-            address: "KT1XukBmtNUW1B7mjhNmMNBYQpxZtmxK5Jpo",
-            author: "pifragile",
-        },
-        {
-            name: "Chroma Rush",
-            address: "KT1WmnsefxSqseDkF5vwbNsADgFX5UyuBSrc",
-            author: "WootScoot",
-        },
-    ];
+    contractList = [];
 } else if (ENV === "prod") {
     contractList = [
         {
             name: "Dash",
             address: "KT1F8NDkP6Ewu5fv4D7Fy4BXUVWngSCsnqKv",
             author: "loackme",
-            showArtifact: true
+            showArtifact: true,
         },
         {
             name: "Shadows",
