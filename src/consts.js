1bb549475df57f81968b7eb303a447d966ff35f5
pifragile <pigu@bluewin.ch>
2024-11-17 13:04:29 +0800

    api node

--- src/consts.js
+++ src/consts.js
@@ -1,6 +1,7 @@
 export const TEZOS_NETWORK = process.env.REACT_APP_TEZOS_NETWORK || 'ghostnet';
 export const TZKT_API = process.env.REACT_APP_TZKT_API || 'https://api.ghostnet.tzkt.io/';
-export const RPC_NODE = process.env.REACT_APP_RPC_NODE || 'https://ghostnet.tezos.marigold.dev/';
+export const RPC_NODE = process.env.REACT_APP_RPC_NODE || 'https://ghostnet.smartpy.io';
+// export const RPC_NODE = process.env.REACT_APP_RPC_NODE || 'https://ghostnet.tezos.marigold.dev/';
 export const OBJKT_API = 'https://data.objkt.com/v3/graphql'
 export const IPFS_GATEWAY = 'https://pifragile.mypinata.cloud/ipfs/'
 export const IPFS_UPLOADER_GATEWAY = 'https://editartipfsgateway-yj23z.ondigitalocean.app/'
