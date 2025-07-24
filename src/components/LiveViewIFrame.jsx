e2c5e1acda417dca1dffa1614b23fe9d1f57e71a
pifragile <pigu@bluewin.ch>
2025-07-24 23:23:33 +0800

    fix live view iframe

--- src/components/LiveViewIFrame.jsx
+++ src/components/LiveViewIFrame.jsx
@@ -1,8 +1,31 @@
+
+import { useEffect, useRef, useState } from "react";
 import { resolveIpfsGatewaySketches, resolveIpfsSketches } from "../lib/utils";
 
 function LiveViewIFrame({ url }) {
+    const iframeRef = useRef(null);
+    const [iframeSrc, setIframeSrc] = useState("");
+
+    useEffect(() => {
+        function checkDimensions() {
+            const iframe = iframeRef.current;
+            if (iframe) {
+                const { width, height } = iframe.getBoundingClientRect();
+                if (width > 0 && height > 0) {
+                    setIframeSrc(resolveIpfsSketches(url));
+                } else {
+                    // Try again on next frame
+                    requestAnimationFrame(checkDimensions);
+                }
+            }
+        }
+        checkDimensions();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [url]);
+
     return (
         <iframe
+            ref={iframeRef}
             title="token"
             id="tokenFrame"
             style={{
@@ -10,7 +33,7 @@ function LiveViewIFrame({ url }) {
                 width: "100%",
                 aspectRatio: "1/1"
             }}
-            src={resolveIpfsSketches(url)}
+            src={iframeSrc}
         />
     );
 }
