b8afd56b1ec94f98dbd5c74699aefea5f3429b5a
pifragile <pigu@bluewin.ch>
2025-07-31 19:20:57 +0800

    tweet

--- src/tweets.jsx
+++ src/tweets.jsx
@@ -1,6 +1,7 @@
 export const tweets = [
     // 'https://x.com/tezos/status/1923397338879639769',
     //'https://x.com/kevinabosch/status/1884950003141009739?s=46&t=6U_gGeDuevYiLBQ-UCHFTA',
+    'https://x.com/GenerativePunk/status/1950871488560611450',
     'https://x.com/sutanz/status/1950280035279012143',
     'https://x.com/TenebrisVia/status/1948647793276715389',
     'https://x.com/etsuranpazz/status/1948270457645670744?s=46&t=6U_gGeDuevYiLBQ-UCHFTA',
